5c1285deb1eaca453c8920de1324cc9502ae0731
ManuGS <manugsueyro8@gmail.com>
2025-08-05 21:37:18 -0300

    Version 1

---
+++ script.js
@@ -0,0 +1,42 @@
+
+const playerContainer=document.querySelectorAll(".player-container")
+const playerInput=document.querySelector(".playerName");
+const options=document.querySelectorAll(".option")
+let playerName;
+playerInput.addEventListener("input",()=>{
+    if(playerInput!=="") playerName=playerInput.value;
+    else return
+})
+
+playerContainer.forEach((player)=>{
+    player.addEventListener("click",()=>{
+        if(/^[a-zA-Z ]+$/.test(playerName)){
+            player.textContent=playerName
+            playerInput.value=""
+        } else return
+        
+    })
+})
+
+options.forEach((option)=>{
+    option.addEventListener("input",()=>{
+        if(option.value=="mco") document.querySelector(".pitch-container").style.gridTemplateAreas = `
+                                                                                        ". . dc . ."
+                                                                                        "ei . . . ed"
+                                                                                        ". . mco . ."
+                                                                                        ". mc2 . mc1 ."
+                                                                                        "li . . . ld"
+                                                                                        ". dfc2 . dfc1 ."
+                                                                                        ". . arq . ."
+                                                                                        `;
+        else document.querySelector(".pitch-container").style.gridTemplateAreas=`
+                                                                                        ". . dc . ."
+                                                                                        "ei . . . ed"
+                                                                                        ". mc2 . mc1 ."
+                                                                                        ". . mco . ."
+                                                                                        "li . . . ld"
+                                                                                        ". dfc2 . dfc1 ."
+                                                                                        ". . arq . ."
+                                                                                        `;
+    })
+})
